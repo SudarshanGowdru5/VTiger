33f89f81356276d5164e25068e28839e98ce6512
Devraj <Devraj@Devraj-PC>
2021-09-01 12:46:23 +0530

    update in ExcelFile

--- VTiger/src/test/java/organization/TC_1Test.java
+++ VTiger/src/test/java/organization/TC_1Test.java
@@ -4,17 +4,20 @@ import java.util.Random;
 
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import genericLibrary.BaseTest;
+import genericLibrary.ExcelUtil;
 import pomRepository.CreateNewOrganizationPage;
 import pomRepository.HomePage;
 import pomRepository.OrganizationsPage;
 
+
+
+
 public class TC_1Test extends BaseTest {
 	@Test
-	public void createOrganization() throws InterruptedException {
+	public void createOrganization() throws Exception {
 		HomePage homePage=new HomePage(driver);
 		Reporter.log("Execuiting TC_1",true);
 		homePage.getOrganizationTab().click();
@@ -23,12 +26,13 @@ public class TC_1Test extends BaseTest {
 		organizationPage.getCreateOrganization().click();
 		Reporter.log("Sucessfully createOrganization page is Displayed",true);
 		CreateNewOrganizationPage createNewOrganization=new CreateNewOrganizationPage(driver);
+		ExcelUtil util = new ExcelUtil();
 		String name="TestYantra";
 		Random rnd=new Random();
 		int rnm = rnd.nextInt(1000);
 		createNewOrganization.getOrganizationName().clear();
-		createNewOrganization.getOrganizationName().sendKeys(name+rnm);
-		Assert.assertEquals(name+rnm,createNewOrganization.getOrganizationName().getAttribute("value"), "entered Value is Wrong");
+		createNewOrganization.getOrganizationName().sendKeys(util.excelStringFile("Sheet1", 0, 0)+rnm);
+		Assert.assertEquals(util.excelStringFile("Sheet1", 0, 0)+rnm,createNewOrganization.getOrganizationName().getAttribute("value"), "entered Value is Wrong");
 		Reporter.log("Sucessfully entered the data into the organization TextField",true);
 		createNewOrganization.getSave().click();
 		Thread.sleep(1000);
